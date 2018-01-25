47cacad7d96e563380252f842cd1bbd253a3339e
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-01-25 11:12:49 -0500

    consumos de api con URL de ejemplo
    -solo se creo el controlador ArticuloACM
    -la vista busquedaArxiv solo presenta los datos de la api de prueba

--- App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/ArticuloACMController.js
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/ArticuloACMController.js
@@ -1,11 +1,2 @@
-/**
- * ArticuloACMController
- *
- * @description :: Server-side logic for managing Articuloacms
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-
-module.exports = {
-	
-};
-
+//    localhost:1337/Articulo/metodos
+module.exports = {};
---
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/ArticuloACMController.ts
@@ -0,0 +1,10 @@
+declare var module;
+declare var sails;
+declare var Articulo;
+
+//    localhost:1337/Articulo/metodos
+
+module.exports = {
+
+
+}
