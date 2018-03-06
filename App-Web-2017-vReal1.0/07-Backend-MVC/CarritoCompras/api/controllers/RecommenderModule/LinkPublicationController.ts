72ee5232464377427c20bb3441cd9156d02ffc30
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-03-06 13:16:38 -0500

    link

--- App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/LinkPublicationController.ts
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/LinkPublicationController.ts
@@ -6,24 +6,31 @@ declare var Articulo;
 
 module.exports = {
 
-  crearLinkPublication: (req, res) => {
 
-    let parametros = req.allParams();
-    let nuevoLinks = {
-      links_Value: parametros.links_Value,
-      link_Type: parametros.link_Type,
-      o_Value: parametros.o_Value,
-      o_Type: parametros.o_Type,
+  test:(req, res) =>{
+
+    return res.view('RecommenderModule/byLink')
+  },
+
+
+  createLinkToAPublication: (req, res) => {
+
+    let parameters = req.allParams();
+    let newLinks = {
+      links_Value: parameters.links_Value,
+      link_Type: parameters.link_Type,
+      o_Value: parameters.o_Value,
+      o_Type: parameters.o_Type,
     };
-    LinkPublication.create(nuevoLinks)
+    LinkPublication.create(newLinks)
       .exec(
-        (error,linkCreado)=>{
+        (error,linkcreated)=>{
           if(error){
             return res.serverError(error);
           }else{
             // return res.ok(linkCreado);
             //return res.created('Nuevo articulo creado.');
-            return res.view('recommender')
+            return res.view('RecommenderModule/byLink')
           }
         }
       )
