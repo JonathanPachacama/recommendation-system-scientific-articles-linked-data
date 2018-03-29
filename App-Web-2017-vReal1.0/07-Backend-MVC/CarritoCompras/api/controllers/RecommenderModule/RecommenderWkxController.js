0c55b9bd0b901ec3628e51425e5ea22ea36278ca
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-03-29 13:13:28 -0500

    prueba traer metadatos de Wikidx
    Arreglar la condicion del IF solo funciona los 3 primeros

--- App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.js
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.js
@@ -4,45 +4,52 @@ module.exports = {
     },
     recommenderWkx: function (req, res) {
         //let CollectionId:number[] = [];
-        Wkx_collection.find()
-            .exec(function (error, collectionFound) {
-            if (error) {
-                return res.serverError(error);
-            }
-            else {
-                for (var i = 0; i < collectionFound.length; i++) {
-                    CollectionId = collectionFound[i].collectionId;
-                    console.log("CollectionId:", CollectionId);
-                }
-                Wkx_publisher.find()
-                    .exec(function (error, publisherFound) {
-                    if (error) {
-                        return res.serverError(error);
-                    }
-                    else {
-                        for (var i = 0; i < collectionFound.length; i++) {
-                            PublisherId = publisherFound[i].publisherId;
-                            console.log("publisherFound:", PublisherId);
-                        }
-                        Wkx_resource_misc.find({
-                            resourcemiscCollection: CollectionId,
-                            resourcemiscPublisher: PublisherId,
-                        })
-                            .exec(function (error, resource_miscFound) {
-                            if (error) {
-                                return res.serverError(error);
-                            }
-                            else {
-                                return res.view('RecommenderModule/MainWikindx', {
-                                    resourcemisc: resource_miscFound
-                                });
-                                // return res.ok(resource_miscFound)
-                            }
-                        });
-                    }
-                });
-            }
+        Wkx_creator
+            .find()
+            .exec(function (err, creatorsFound) {
+            if (err)
+                return res.negotiate(err);
+            console.log("Article:", creatorsFound);
+            return res.view('RecommenderModule/MainWikindx', {
+                creators: creatorsFound
+            });
         });
+        // Wkx_collection.find().exec((error,collectionFound)=>{
+        //       if(error){
+        //         return res.serverError(error);
+        //       }
+        //       else{
+        //         for(let i = 0;i<collectionFound.length;i ++){
+        //           CollectionId=collectionFound[i].collectionId
+        //           console.log("CollectionId:", CollectionId)
+        //         }
+        //
+        //         Wkx_publisher.find().exec((error,publisherFound)=>{
+        //               if(error){
+        //                 return res.serverError(error);
+        //               }else{
+        //                 for(let i = 0;i<collectionFound.length;i ++){
+        //                   PublisherId=publisherFound[i].publisherId
+        //                   console.log("publisherFound:", PublisherId)
+        //                 }
+        //                 Wkx_resource_misc.find({resourcemiscCollection:CollectionId, resourcemiscPublisher: PublisherId,}).exec((error,resource_miscFound)=>{
+        //                       if(error){
+        //                         return res.serverError(error);
+        //                       }
+        //                       else{
+        //                         return res.view('RecommenderModule/MainWikindx',{
+        //                           resourcemisc:resource_miscFound
+        //                         })
+        //
+        //
+        //                         // return res.ok(resource_miscFound)
+        //                       }
+        //                     });
+        //               }
+        //
+        //             });
+        //       }
+        //     });
     },
     bringParametersCreator: function (req, res) {
         var parameters = req.allParams();
@@ -55,12 +62,29 @@ module.exports = {
                     return res.serverError(err);
                 if (creatorFound) {
                     //Si encontro
-                    var Firstname = creatorFound.creatorFirstname;
-                    var Surname = creatorFound.creatorSurname;
-                    sails.log.info("Nombre:", Firstname);
-                    sails.log.info("Apellido:", Surname);
-                    return res.view('RecommenderModule/wkx_creator', {
-                        creator: creatorFound
+                    Wkx_creator.query('SELECT creatorId,creatorFirstname,creatorSurname,resourceTitle,categoryCategory,keywordKeyword\n' +
+                        'FROM wkx_resource,wkx_creator,wkx_resource_creator,wkx_category,wkx_resource_category,wkx_keyword,wkx_resource_keyword\n' +
+                        'WHERE wkx_creator.creatorId=wkx_resource_creator.resourcecreatorId AND wkx_resource.resourceId=wkx_resource_creator.resourcecreatorResourceId\n' +
+                        'AND(wkx_category.categoryId=wkx_resource_category.resourcecategoryCategoryId AND wkx_resource_category.resourcecategoryResourceId=wkx_resource.resourceId)\n' +
+                        'AND (wkx_keyword.keywordId=wkx_resource_keyword.resourcekeywordKeywordId AND wkx_resource_keyword.resourcekeywordResourceId=wkx_resource.resourceId)\n', function (err, rawResult) {
+                        if (err) {
+                            return res.serverError(err);
+                        }
+                        if (creatorFound.creatorId == rawResult[creatorFound.creatorId - 1].creatorId) {
+                            sails.log("id de la Query", rawResult[0].creatorId);
+                            sails.log("id del find", creatorFound.creatorId);
+                            sails.log("pRUEBA", rawResult[creatorFound.creatorId - 1].creatorId);
+                            return res.view('RecommenderModule/wkx_creator', {
+                                creator: creatorFound,
+                                query: rawResult
+                            });
+                        }
+                        else {
+                            return res.view('RecommenderModule/wkx_creator', {
+                                creator: creatorFound,
+                                query: rawResult
+                            });
+                        }
                     });
                 }
                 else {
--- App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.ts
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.ts
@@ -22,63 +22,68 @@ module.exports = {
 
   recommenderWkx:(req,res)=>{
 
+
     //let CollectionId:number[] = [];
 
 
-    Wkx_collection.find()
-      .exec(
-        (error,collectionFound)=>{
-          if(error){
-            return res.serverError(error);
-          }else{
+    Wkx_creator
+      .find()
+      .exec((err,creatorsFound)=>{
+        if(err) return res.negotiate(err);
+        console.log("Article:",creatorsFound)
 
-            for(let i = 0;i<collectionFound.length;i ++){
-              CollectionId=collectionFound[i].collectionId
-              console.log("CollectionId:", CollectionId)
-            }
-
-            Wkx_publisher.find()
-              .exec(
-                (error,publisherFound)=>{
-                  if(error){
-                    return res.serverError(error);
-                  }else{
-                    for(let i = 0;i<collectionFound.length;i ++){
-                      PublisherId=publisherFound[i].publisherId
-                      console.log("publisherFound:", PublisherId)
-                    }
-
-                    Wkx_resource_misc.find({
-                      resourcemiscCollection:CollectionId,
-                      resourcemiscPublisher: PublisherId,
-                    })
-                      .exec(
-                        (error,resource_miscFound)=>{
-                          if(error){
-                            return res.serverError(error);
-                          }
-
-                          else{
-                            return res.view('RecommenderModule/MainWikindx',{
-                              resourcemisc:resource_miscFound
-
-                            })
+        return res.view('RecommenderModule/MainWikindx',{
+          creators:creatorsFound
+        })
+      });
 
 
-                            // return res.ok(resource_miscFound)
-                          }
-                        }
-
-                      )
 
 
-                  }
 
-                }
-              )
-          }
-        }
-      )
+
+
+
+
+
+
+
+    // Wkx_collection.find().exec((error,collectionFound)=>{
+    //       if(error){
+    //         return res.serverError(error);
+    //       }
+    //       else{
+    //         for(let i = 0;i<collectionFound.length;i ++){
+    //           CollectionId=collectionFound[i].collectionId
+    //           console.log("CollectionId:", CollectionId)
+    //         }
+    //
+    //         Wkx_publisher.find().exec((error,publisherFound)=>{
+    //               if(error){
+    //                 return res.serverError(error);
+    //               }else{
+    //                 for(let i = 0;i<collectionFound.length;i ++){
+    //                   PublisherId=publisherFound[i].publisherId
+    //                   console.log("publisherFound:", PublisherId)
+    //                 }
+    //                 Wkx_resource_misc.find({resourcemiscCollection:CollectionId, resourcemiscPublisher: PublisherId,}).exec((error,resource_miscFound)=>{
+    //                       if(error){
+    //                         return res.serverError(error);
+    //                       }
+    //                       else{
+    //                         return res.view('RecommenderModule/MainWikindx',{
+    //                           resourcemisc:resource_miscFound
+    //                         })
+    //
+    //
+    //                         // return res.ok(resource_miscFound)
+    //                       }
+    //                     });
+    //               }
+    //
+    //             });
+    //       }
+    //     });
 
   },
 
@@ -94,15 +99,36 @@ module.exports = {
           if(creatorFound){
             //Si encontro
 
-            let Firstname=creatorFound.creatorFirstname;
-            let Surname=creatorFound.creatorSurname;
+            Wkx_creator.query('SELECT creatorId,creatorFirstname,creatorSurname,resourceTitle,categoryCategory,keywordKeyword\n' +
+              'FROM wkx_resource,wkx_creator,wkx_resource_creator,wkx_category,wkx_resource_category,wkx_keyword,wkx_resource_keyword\n' +
+              'WHERE wkx_creator.creatorId=wkx_resource_creator.resourcecreatorId AND wkx_resource.resourceId=wkx_resource_creator.resourcecreatorResourceId\n' +
+              'AND(wkx_category.categoryId=wkx_resource_category.resourcecategoryCategoryId AND wkx_resource_category.resourcecategoryResourceId=wkx_resource.resourceId)\n' +
+              'AND (wkx_keyword.keywordId=wkx_resource_keyword.resourcekeywordKeywordId AND wkx_resource_keyword.resourcekeywordResourceId=wkx_resource.resourceId)\n' ,function(err, rawResult) {
+              if (err) { return res.serverError(err); }
 
-            sails.log.info("Nombre:",Firstname);
-            sails.log.info("Apellido:",Surname);
-            return res.view('RecommenderModule/wkx_creator',{
+              if(creatorFound.creatorId == rawResult[creatorFound.creatorId-1].creatorId){
 
-              creator:creatorFound
-            })
+                sails.log("id de la Query",rawResult[0].creatorId);
+                sails.log("id del find",creatorFound.creatorId);
+                sails.log("pRUEBA",rawResult[creatorFound.creatorId-1].creatorId);
+                return res.view('RecommenderModule/wkx_creator',{
+                  creator:creatorFound,
+                  query:rawResult
+                })
+              }
+
+              else{
+
+                return res.view('RecommenderModule/wkx_creator',{
+                  creator:creatorFound,
+                  query:rawResult
+                })
+
+              }
+            });
+
+
+
           }else{
             //No encontro
             return res.redirect('/')
@@ -111,9 +137,6 @@ module.exports = {
     }else{
       return res.redirect('/')
     }
-
-
-
 
   }
 
