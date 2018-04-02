15aa608dcf3c75a478802e1f67c1a4a45f30e516
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-04-02 13:13:43 -0500

    extracion metadatos wikindx1
    Falta agregar todas las keywords y todas las categorias en la vista

--- App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.js
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.js
@@ -82,9 +82,21 @@ module.exports = {
                             });
                         }
                         else {
-                            return res.view('RecommenderModule/wkx_creator', {
-                                creator: creatorFound,
-                                query: rawResult
+                            Wkx_creator.query('SELECT creatorId,creatorFirstname,creatorSurname,resourceId,resourceTitle,categoryId,categoryCategory,keywordId,keywordKeyword\n' +
+                                'FROM wkx_resource,wkx_creator,wkx_resource_creator,wkx_category,wkx_resource_category,wkx_keyword,wkx_resource_keyword\n' +
+                                'WHERE wkx_creator.creatorId=wkx_resource_creator.resourcecreatorId AND wkx_resource.resourceId=wkx_resource_creator.resourcecreatorResourceId\n' +
+                                'AND(wkx_category.categoryId=wkx_resource_category.resourcecategoryCategoryId AND wkx_resource_category.resourcecategoryResourceId=wkx_resource.resourceId)\n' +
+                                'AND (wkx_keyword.keywordId=wkx_resource_keyword.resourcekeywordKeywordId AND wkx_resource_keyword.resourcekeywordResourceId=wkx_resource.resourceId)\n' +
+                                'AND (wkx_creator.creatorId=?)', [creatorFound.creatorId], function (err, rawResult2) {
+                                if (err) {
+                                    return res.serverError(err);
+                                }
+                                sails.log("rawResult", rawResult2);
+                                sails.log("creatorFound", creatorFound);
+                                return res.view('RecommenderModule/wkx_keyword', {
+                                    creator2: creatorFound,
+                                    query2: rawResult2
+                                });
                             });
                         }
                     });
