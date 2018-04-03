c41070fb0ac5bfce119ebb642b73bdd8f237104c
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-04-03 13:19:44 -0500

    extracion metadatos wikindx2
    Falta arreglar que no se repitan  las keywords y  las categorias

--- App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.ts
+++ App-Web-2017-vReal1.0/07-Backend-MVC/CarritoCompras/api/controllers/RecommenderModule/RecommenderWkxController.ts
@@ -104,43 +104,91 @@ module.exports = {
               'FROM wkx_resource,wkx_creator,wkx_resource_creator,wkx_category,wkx_resource_category,wkx_keyword,wkx_resource_keyword\n' +
               'WHERE wkx_creator.creatorId=wkx_resource_creator.resourcecreatorId AND wkx_resource.resourceId=wkx_resource_creator.resourcecreatorResourceId\n' +
               'AND(wkx_category.categoryId=wkx_resource_category.resourcecategoryCategoryId AND wkx_resource_category.resourcecategoryResourceId=wkx_resource.resourceId)\n' +
-              'AND (wkx_keyword.keywordId=wkx_resource_keyword.resourcekeywordKeywordId AND wkx_resource_keyword.resourcekeywordResourceId=wkx_resource.resourceId)' ,function(err, rawResult) {
+              'AND (wkx_keyword.keywordId=wkx_resource_keyword.resourcekeywordKeywordId AND wkx_resource_keyword.resourcekeywordResourceId=wkx_resource.resourceId)\n' +
+              'AND (wkx_creator.creatorId=?)' , [ creatorFound.creatorId ] ,function(err, rawResult2) {
               if (err) { return res.serverError(err); }
 
-              if(creatorFound.creatorId == rawResult[creatorFound.creatorId-1].creatorId){
-                sails.log("rawResult",rawResult);
+
+              if(rawResult2.length!= 1){
+
+                sails.log("tamaño",rawResult2.length);
+
+                var query = [];
+                //query[0] = rawResult2[0]
+                //sails.log("query[0]",query[0]);
+                let iteracion = [];
+
+                let keyword = []
+                let category = []
+                for (let i = 0; i < rawResult2.length;i++){
+
+                  if(rawResult2[i]. creatorId == creatorFound.creatorId){
+                    iteracion.push( rawResult2[i]);
+                    keyword.push( rawResult2[i].keywordKeyword);
+                    category.push( rawResult2[i].categoryCategory);
+                  }
+
+                }
+
+                query = iteracion;
+                sails.log("query ",query);
+
+                sails.log("keyword ",keyword);
+                sails.log("category ",category);
+
+                sails.log("category ",query[0]);
+
+                return res.view('RecommenderModule/wkx_keyword',{
+                  creator:creatorFound,
+                  query:query[0],
+                  keyword:keyword,
+                  category:category
+                })
+
+                // return res.ok()
+
+
+
+
+
+
+  ///////////////////////ejemplo
+//                 var data = [];
+// // ...
+//                 data[0] = { "ID": "1", "Status": "Valid" };
+//                 data[1] = { "ID": "2", "Status": "Invalid" };
+// // ...
+//                 var tempData = [];
+//                 for ( var index=0; index<data.length; index++ ) {
+//                   if ( data[index].Status == "Valid" ) {
+//                     tempData.push( data );
+//                   }
+//                 }
+//                 data = tempData;
+
+
+
+
+
+
+
+              }
+
+              else {
+                sails.log("rawResult",rawResult2);
                 sails.log("creatorFound",creatorFound);
-                sails.log("id de la Query",rawResult[0].creatorId);
-                sails.log("id del findonOneCreator",creatorFound.creatorId);
-                sails.log("pRUEBA",rawResult[creatorFound.creatorId-1].creatorId);
+
+                let query = rawResult2[0]
+
                 return res.view('RecommenderModule/wkx_creator',{
                   creator:creatorFound,
-                  query:rawResult
+                  query:query
                 })
+
+
               }
 
-              else{
 
-
-
-                Wkx_creator.query('SELECT creatorId,creatorFirstname,creatorSurname,resourceId,resourceTitle,categoryId,categoryCategory,keywordId,keywordKeyword\n' +
-                  'FROM wkx_resource,wkx_creator,wkx_resource_creator,wkx_category,wkx_resource_category,wkx_keyword,wkx_resource_keyword\n' +
-                  'WHERE wkx_creator.creatorId=wkx_resource_creator.resourcecreatorId AND wkx_resource.resourceId=wkx_resource_creator.resourcecreatorResourceId\n' +
-                  'AND(wkx_category.categoryId=wkx_resource_category.resourcecategoryCategoryId AND wkx_resource_category.resourcecategoryResourceId=wkx_resource.resourceId)\n' +
-                  'AND (wkx_keyword.keywordId=wkx_resource_keyword.resourcekeywordKeywordId AND wkx_resource_keyword.resourcekeywordResourceId=wkx_resource.resourceId)\n' +
-                  'AND (wkx_creator.creatorId=?)' , [ creatorFound.creatorId ] ,function(err, rawResult2) {
-                  if (err) { return res.serverError(err); }
-
-                  sails.log("rawResult",rawResult2);
-                  sails.log("creatorFound",creatorFound);
-                  return res.view('RecommenderModule/wkx_keyword',{
-                    creator2:creatorFound,
-                    query2:rawResult2
-                  })
-
-                });
-
-              }
             });
 
 
