ed9af078aaf4b4e596037920230c6b23b9721775
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-04-24 11:06:07 -0500

    carpetas innecesarias eliminadas

--- App-Web-2017-vReal1.0/05-Typescript/Ecmascript/Map.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by USRDEL on 29/5/17.
- */
-var arreglo = [4, 6, 7, 4, 5, 8, 10, 4, 7];
-// Me devuelve un arreglo
-var resultado = arreglo.map(function (valor, indice, arreglo) {
-    return (valor * 2) / 10;
-});
-console.log(resultado);
