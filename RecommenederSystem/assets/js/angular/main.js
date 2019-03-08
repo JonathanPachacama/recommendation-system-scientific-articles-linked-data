d41cafd559c876f3f9872a940e370842b37caabe
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2019-03-08 00:23:58 -0500

    cors proxy thingproxy.freeboard.io

--- RecommenederSystem/assets/js/angular/main.js
+++ RecommenederSystem/assets/js/angular/main.js
@@ -4,24 +4,32 @@ var angularApp = angular.module('appRecommenderLayout', ['ui.bootstrap']);
 
 angularApp.controller("layoutController",function ($scope,$http) {
 }).factory('url_config', function() {
-  var base_url = 'http://localhost:1337/RecommenderModule/'
-  http://localhost:1337/RecommenderModule/RecommenderWkx/bringParametersCreatorAPI?resourceId=2
+  var base_urlSails = 'http://localhost:1337'
+  var base_UrlWebServer = 'http://localhost:8080'
+  // var base_urlSails = 'http://192.168.1.6:1337'
+  // var base_UrlWebServer = 'http://192.168.1.6:8080'
+
+
+  var urlLuceneIEEE = base_UrlWebServer+"/JavaAPI/api/articulos/getBusquedaIEEE/?Busqueda="
+  var urlLuceneACM = base_UrlWebServer+"/JavaAPI/api/articulos/getBusquedaACM/?Busqueda="
+  var urlLuceneDBLP = base_UrlWebServer+"/JavaAPI/api/articulos/getBusquedaDBLP/?Busqueda="
+  //http://localhost:1337/RecommenderModule/RecommenderWkx/bringParametersCreatorAPI?resourceId=2
     return {
-      base_url : base_url,
+      base_urlSails : base_urlSails,
+      base_UrlWebServer : base_UrlWebServer,
+      urlLuceneIEEE : urlLuceneIEEE,
+      urlLuceneACM : urlLuceneACM,
+      urlLuceneDBLP : urlLuceneDBLP,
     };
 });
 
-angularApp.controller("recommenderController",function ($scope,$http, $log) {
+angularApp.controller("recommenderController",function ($scope,$http, $log, url_config) {
 
-  let urlLuceneIEEE = "http://localhost:8080/JavaAPI/api/articulos/getBusquedaIEEE/?Busqueda="
-  let urlLuceneACM = "http://localhost:8080/JavaAPI/api/articulos/getBusquedaACM/?Busqueda="
-  let urlLuceneDBLP = "http://localhost:8080/JavaAPI/api/articulos/getBusquedaDBLP/?Busqueda="
   // let TitleLucene = $("#title").val();    //get value of keywords of the title
   let fullTitle = $("#fullTitle").text();    //get value of full title
-  let urlSendIEEE = urlLuceneIEEE+fullTitle
-  let urlSendACM = urlLuceneACM+fullTitle
-  let urlSendDBLP = urlLuceneDBLP+fullTitle
-
+  let urlSendIEEE = url_config.urlLuceneIEEE+fullTitle
+  let urlSendACM = url_config.urlLuceneACM+fullTitle
+  let urlSendDBLP = url_config.urlLuceneDBLP+fullTitle
   $scope.showIEEE;
   $scope.showACM;
   $scope.showDBLP;
@@ -36,8 +44,7 @@ angularApp.controller("recommenderController",function ($scope,$http, $log) {
         $("#acmNav").attr("class","nav-item disabled");
         $("#dblpNav").attr("class","nav-item disabled");
         $scope.responseJsonLucene = data;
-        $scope.endPointDinamic = "http://acm.rkbexplorer.com/sparql/";
-        console.log("la url es:" + urlSendIEEE);
+        console.log("la url es:" +  urlSendIEEE);
         console.log("numero de articulos:" + $scope.responseJsonLucene.length);
         for (let i = 0; i < $scope.responseJsonLucene.length; i++) {
           let TitleLucene = $scope.responseJsonLucene[i].tituloArticulo
@@ -175,8 +182,7 @@ angularApp.controller('mainWikindxController',
 
       $scope.buscarArticulo = function () {
 
-        var url = url_config.base_url+'recommenderWkx/recommenderWkxAPI?busqueda='+$scope.busqueda
-
+        var url = url_config.base_urlSails+'/RecommenderModule/recommenderWkx/recommenderWkxAPI?busqueda='+$scope.busqueda
 
         console.log('url',url);
         $http.get(url)
@@ -222,7 +228,7 @@ angularApp.controller('mainWikindxController',
 
       $scope.irRecomendacion = function () {
 
-        var url = url_config.base_url+'RecommenderWkx/bringParametersCreatorAPI?resourceId='+$scope.resourceId
+        var url = url_config.base_urlSails+'RecommenderWkx/bringParametersCreatorAPI?resourceId='+$scope.resourceId
 
 
         console.log('url',url);
@@ -277,3 +283,62 @@ angularApp.controller('mainWikindxController',
       return input.slice(start);
     }
   });
+
+angularApp.controller("Controller",function ($scope,$http) {
+
+  $scope.responseJson= [];
+
+  var prefix = "PREFIX acm: <http://acm.rkbexplorer.com/id/>\n"+
+    "PREFIX ieee: <http://ieee.rkbexplorer.com/id/>\n"+
+    "PREFIX dblp: <http://dblp.rkbexplorer.com/id/>\n"+
+    "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+    "PREFIX akt:  <http://www.aktors.org/ontology/portal#>\n" +
+    "PREFIX owl:  <http://www.w3.org/2002/07/owl#>\n" +
+    "PREFIX iai: <http://www.iai.uni-sb.de/resist#>\n"+
+    "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n"+
+    "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+    "PREFIX akts: <http://www.aktors.org/ontology/support#>\n"+
+    "PREFIX unlocode: <http://unlocode.rkbexplorer.com/id/>\n" +
+    "PREFIX class: <http://acm.rkbexplorer.com/ontologies/acm#>\n"+
+    "PREFIX extension: <http://www.aktors.org/ontology/extension#>\n\n"
+
+
+
+  var querySparql = "SELECT DISTINCT ?links ?o\n" +
+    "WHERE {<http://acm.rkbexplorer.com/id/100233> ?links ?o}\n" +
+    "Limit 20"
+
+  let endPointAcm ="http://acm.rkbexplorer.com/sparql/";
+  let queryParametersSparql = "?format=json&query=";
+  let apiAcm = endPointAcm + queryParametersSparql  //api de ACM con sus parametros
+
+
+  let query = prefix+querySparql
+
+  let concatenation = apiAcm+encodeURIComponent(query);
+
+  $http.get("https://thingproxy.freeboard.io/fetch/"+concatenation)
+    .success(function (data){
+      console.log(data);
+      $scope.responseJson = data.results.bindings;
+      console.log("la url es:" + concatenation);
+      console.log("numero de articulos:" + $scope.responseJson.length);
+      for(let i = 0;i<$scope.responseJson.length;i ++){
+        let linksValue = $scope.responseJson[i].links.value
+        let linksType = $scope.responseJson[i].links.type
+        let oValue = $scope.responseJson[i].o.value
+        let oType = $scope.responseJson[i].o.type
+
+        console.log(i+1+": valor del link es:"+ linksValue);
+        console.log(i+1+": tipo del link es:"+ linksType);
+        console.log(i+1+": valor del o es:"+ oValue);
+        console.log(i+1+": tipo del o es:"+ oType);
+
+      }
+    })
+    .error(function (err) {
+    });
+
+
+});
