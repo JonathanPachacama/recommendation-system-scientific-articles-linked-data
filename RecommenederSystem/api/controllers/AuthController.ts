8a9f131e034234652245192d46b7eb9fb7b1c384
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2019-04-05 08:23:54 -0500

    layout session

--- RecommenederSystem/api/controllers/AuthController.ts
+++ RecommenederSystem/api/controllers/AuthController.ts
@@ -38,7 +38,7 @@ module.exports = {
       user_last_name:parametros.user_last_name,
       user_email:parametros.user_email,
       user_password:parametros.user_password,
-      user_profile_completed:0,
+      user_has_access:1,
       user_username:parametros.user_username
     };
 
@@ -61,7 +61,7 @@ module.exports = {
               pro_art_id:userCreated.user_id,
               pro_path_photo:'theme/dist/img/user-avatar.jpg',
               pro_phone:parametros.user,
-              pro_has_access:1,
+              pro_completed:0,
               user_date_created:new Date('Y-m-d H:i:s'),
               user_date_updated:new Date('Y-m-d H:i:s'),
             };
@@ -139,38 +139,67 @@ module.exports = {
                   });
                 },
                 success: function () {
-                  req.session.authenticated = true;
+
+                  let query = 'select user.*,rol_users.rol_name as rol, profile.*\n' +
+                    'from user, rol_users, profile\n' +
+                    'WHERE rol_users.rol_id = user.user_rol_id\n' +
+                    'AND user.user_id = profile.pro_user_id\n' +
+                    'AND user.user_username = ?\n' +
+                    'AND user.user_password = ?\n' +
+                    'AND user.user_has_access = ?\n' +
+                    'limit 1'
+                  User.query(query, [ foundUser.user_username,foundUser.user_password,'1'] ,function(err, User_Session) {
+                    if (err) { return res.serverError(err); }
+
+                    sails.log('result',User_Session);
+                    sails.log('result tamaño',User_Session.length);
+
+                    if (User_Session.length == 1) {
+
+                      req.session.authenticated = true;
+                      req.session.all = User_Session[0]
+                      req.session.me = {
+                        usuarioId:User_Session[0].user_id,
+                        rol:User_Session[0].rol,
+                        rolId:User_Session[0].user_rol_id,
+                        name:User_Session[0].user_name,
+                        last_name:User_Session[0].user_last_name,
+                        username:User_Session[0].user_username,
+                        email:User_Session[0].user_email,
+                        path_photo:User_Session[0].pro_path_photo,
+                      }
 
 
-                  req.session.me = {
-                    id:foundUser.user_id,
-                    rol:foundUser.user_rol_id,
-                    name:foundUser.user_name,
-                    last_name:foundUser.user_last_name,
-                    email:foundUser.user_email,
-                    perfil:foundUser.user_profile_completed,
-                    username:foundUser.user_username
-                  }
+                      console.log("req.session.me",req.session.me);
+                      console.log("req.session.me",req.session.me.user_name);
+                      console.log("req.session.all1",req.session.all);
+                      console.log("req.session.all2",req.session.all.user_username);
 
-                  console.log("req.session.me",req.session.me);
-                  // return the credential
-                  var token =jwt
-                    .sign(
-                      {
-                        exp:Math.floor(Date.now()/ 1000)+(60*60), // expires in one hour
-                        data:{
-                          user_id:foundUser.user_id,
-                          user_name:foundUser.user_name,
-                          user_last_name:foundUser.user_last_name,
-                          user_username:foundUser.user_username,
-                          user_email:foundUser.user_email
-                        }
-                      },
-                      'secret');  //secret word
-                  console.log("token",token);
-                  var decodeToken = TokenService.decode(token);
-                  console.log("helloMessage",decodeToken);
-                  res.redirect('/');
+
+                      // return the credential
+                      var token =jwt
+                        .sign(
+                          {
+                            exp:Math.floor(Date.now()/ 1000)+(60*60), // expires in one hour
+                            data:{
+                              user_id:foundUser.user_id,
+                              user_name:foundUser.user_name,
+                              user_last_name:foundUser.user_last_name,
+                              user_username:foundUser.user_username,
+                              user_email:foundUser.user_email
+                            }
+                          },
+                          'secret');  //secret word
+                      console.log("token",token);
+                      var decodeToken = TokenService.decode(token);
+                      console.log("helloMessage",decodeToken);
+                      res.redirect('/');
+
+
+                    } else {
+                      res.redirect('/');
+                    }
+                  });
 
                 }
               });
