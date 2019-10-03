03485b3f1800bcf36ddd64fb035e15c6bf7b174d
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2019-10-03 06:39:14 -0500

    update ip cloud

--- RecommenederSystem/api/services/IpService.js
+++ RecommenederSystem/api/services/IpService.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   ip: function (options) {
-    // var ip = '35.247.246.97'
-    var ip = 'localhost'
+     var ip = '35.247.246.97'
+    //var ip = 'localhost'
     return ip ;
   }
 };
